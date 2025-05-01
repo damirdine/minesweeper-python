bd731e6e306f383445da80f071c2a37bfdd0bb10
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2025-05-01 17:54:54 +0200

    create art package

---
+++ art/__init__.py
@@ -0,0 +1,6 @@
+from .art import Art
+from .painting import Painting
+from .drawing import Drawing
+from .sculpture import Sculpture
+
+__all__ = ['Art', 'Painting', 'Drawing', 'Sculpture']
---
+++ art/painting.py
@@ -0,0 +1,5 @@
+class Painting:
+    """
+    docstring
+    """
+    pass
